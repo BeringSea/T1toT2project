5f4e6f23698040b77ad684443fa65cf9fa9d0385
BeringSea <dmojicevic@yahoo.com>
2024-09-11 11:42:55 +0200

    added project from spring initializr

---
+++ src/main/java/com/tear/upgrade/t1tot2upgrade/T1tot2upgradeApplication.java
@@ -0,0 +1,13 @@
+package com.tear.upgrade.t1tot2upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1tot2upgradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1tot2upgradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tear/upgrade/t1tot2upgrade/T1tot2upgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tear.upgrade.t1tot2upgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1tot2upgradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
