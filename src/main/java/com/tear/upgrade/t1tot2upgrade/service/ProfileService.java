70dae678485f13199bb4f537f3212624cddb186d
David Mojicevic <>
2024-10-25 16:30:50 +0200

    added javadoc for profile service

--- src/main/java/com/tear/upgrade/t1tot2upgrade/service/ProfileService.java
+++ src/main/java/com/tear/upgrade/t1tot2upgrade/service/ProfileService.java
@@ -2,7 +2,15 @@ package com.tear.upgrade.t1tot2upgrade.service;
 
 import com.tear.upgrade.t1tot2upgrade.dto.ProfileDTO;
 
+import java.util.NoSuchElementException;
+
 public interface ProfileService {
 
+    /**
+     * Retrieves the profile information for the currently logged-in user.
+     *
+     * @return a {@link ProfileDTO} containing the profile details of the logged-in user.
+     * @throws NoSuchElementException if the logged-in user does not have an associated profile.
+     */
     ProfileDTO getProfileForLoggedInUser();
 }
