0dde5effdfebbc5bb73299ec06628b82df9b1bba
David Mojicevic <>
2024-11-14 11:59:18 +0100

    added logs in jwt filter component class

--- src/main/java/com/tear/upgrade/t1tot2upgrade/configuration/JwtFilter.java
+++ src/main/java/com/tear/upgrade/t1tot2upgrade/configuration/JwtFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +20,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
     @Autowired
@@ -37,20 +39,27 @@ public class JwtFilter extends OncePerRequestFilter {
             token = authHeader.substring(7);
             try {
                 username = jwtTokenService.extractUserName(token);
+                log.info("JWT token extracted for user: {}", username);
             } catch (IllegalArgumentException e) {
+                log.error("Unable to get JWT token from request: {}", e.getMessage());
                 throw new RuntimeException("Unable to get JWT token");
             } catch (ExpiredJwtException e) {
+                log.error("JWT token has expired: {}", token);
                 throw new RuntimeException("Jwt token has expired");
             }
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            log.info("JWT token validated successfully for user: {}", username);
             UserDetails userDetails = userDetailService.loadUserByUsername(username);
 
             if (jwtTokenService.validateToken(token, userDetails)) {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                log.info("User authentication successful for: {}", username);
+            } else {
+                log.warn("JWT token validation failed for user: {}", username);
             }
         }
         filterChain.doFilter(request, response);
